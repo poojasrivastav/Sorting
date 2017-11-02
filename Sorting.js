fd6ab17e0c5b705b704aca1822a86fed119a9586
Pooja Srivastava <bioinfor.pooja@gmail.com>
2017-11-02 22:29:45 +0530

    some changes

--- Sorting.js
+++ Sorting.js
@@ -10,10 +10,39 @@ function sort (arr){
         arr[i +1] = toMove;
       }
     }
-    console.log('count', count);
     if(count === 0){
     sorted = true;
     }
   }
   return arr;
 }
+
+
+function mergeSort(arr){
+
+if(arr.length < 2){
+  return arr;
+}
+// //console.log(mergeSort(arr1) + mergeSort(arr2))
+// if(arr.length === 2){
+//   if(arr[0] > arr[1]){
+//     var toMove = arr[0];
+//     arr[0] = arr[1];
+//     arr[1] = toMove;
+//   }
+// }
+var med = Math.floor(arr.length/2);
+
+var arr1 = arr.slice(0,med);
+var arr2 = arr.slice(med);
+
+
+
+return mergeSort(arr1).concat(mergeSort(arr2));
+
+
+
+
+}
+
+
